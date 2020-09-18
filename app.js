08a22d4d78be89c734259e1473a1416a9b2b98dc
Sergei N <cergiy@yandex.ru>
2020-09-18 23:34:34 +0700

    Upload and process csv file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 
 const Controllers = require('./src/controllers');
 
@@ -16,9 +17,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: './tmp/',
+}));
 
 const controllers = Controllers();
 const routes = Router({ controllers });
 app.use('/', routes);
+
+app.use((err, req, res, next) => {
+  console.log(`Unhandled error detected: ${err.message}`);
+  res.status(500).json({ error: '500 - server error' });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: '404 - route not found' });
+});
 
 module.exports = app;
--- src/controllers/deals.js
+++ src/controllers/deals.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 const csv = require('csv-parser');
-const { Readable } = require('stream');
+const fs = require('fs');
 
 module.exports = () => ({
   index: async (req, res, next) => {
@@ -12,20 +12,32 @@ module.exports = () => ({
   },
   create: async (req, res, next) => {
     try {
-      const csvFile = '1,username1,gem1,100,10,2020-09-17 12:50\nusername2,gem2,150,5,2020-09-16 10:04\nusername3,gem3,100,100,2020-09-15 11:12\nusername4,gem4,200,2,2020-09-20 09:29\nusername5,gem5,220,11,2020-09-19 08:45\nusername6,gem6,13,1,2020-09-18 10:49\n';
-      const rows = [];
-      Readable.from([csvFile])
-        .pipe(csv({ headers: ['customer', 'item', 'total', 'quantity', 'date'], strict: true }))
-        .on('error', (error) => res.json({ status: 'Error', Desc: error.message }))
-        .on('data', (row) => {
-          rows.push(row);
-        })
-        .on('end', () => {
-          console.log(rows);
-          res.json({ status: 'OK' });
-        });
+      // console.log(req.files);
+      if (req.files) {
+        const dealFileObj = req.files.deal;
+        if (!dealFileObj) {
+          return res.json({ status: 'Error', Desc: 'No deal file' });
+        }
+
+        if (dealFileObj.mimetype === 'text/csv') {
+          const rows = [];
+          return fs.createReadStream(dealFileObj.tempFilePath)
+            .on('error', (err) => { next(err); })
+            .pipe(csv({ headers: ['customer', 'item', 'total', 'quantity', 'date'], strict: true }))
+            .on('error', (error) => res.json({ status: 'Error', Desc: error.message }))
+            .on('data', (row) => {
+              rows.push(row);
+            })
+            .on('end', () => {
+              console.log(rows);
+              res.json({ status: 'OK' });
+            });
+        }
+        return res.json({ status: 'Error', Desc: 'Not a csv file' });
+      }
+      return res.json({ status: 'Error', Desc: 'No file' });
     } catch (err) {
-      next(err);
+      return next(err);
     }
   },
 });
